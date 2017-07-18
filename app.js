2f4568cecac63cf8efc31892037b7ceb89db0aa2
Roman <movel22@gmail.com>
2017-07-18 10:14:45 +0000

    Update app.js - add geolaction local map

--- app.js
+++ app.js
@@ -1,29 +1,49 @@
-var options = {
-              enableHighAccuracy: true,
-              timeout: 5000,
-              maximumAge: 0
-            };
-            
-            function success(pos) {
-              var crd = pos.coords;
-            
-            //   console.log('Your current position is:');
-            //   console.log(`Latitude : ${crd.latitude}`);
-            //   console.log(`Longitude: ${crd.longitude}`);
-            //   console.log(`More or less ${crd.accuracy} meters.`);
-              
-              var str = 'Your current position is:<br>';
-                str += `Latitude : ${crd.latitude}<br>`;
-                str += `Longitude: ${crd.longitude}<br>`;
-                str += `More or less ${crd.accuracy} meters.<br>`;
-                
-              document.getElementById("p1").innerHTML = str;
+'use strict';
 
-            }
-            
-            function error(err) {
-              console.warn(`ERROR(${err.code}): ${err.message}`);
-            }
-            
-            /* global navigator */
-            navigator.geolocation.getCurrentPosition(success, error, options);
+window.onload = function() {
+  var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+  };
+  
+  window.document.body.style.background = 'tomato';
+  
+  var output = document.getElementById("out");
+  
+  if (!navigator.geolocation){
+    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+  }
+              
+  function success(position) {
+    var crd = position.coords;
+    
+    var str = 'Your current position is:<br>';
+      str += `Latitude : ${crd.latitude}<br>`;
+      str += `Longitude: ${crd.longitude}<br>`;
+      str += `More or less ${crd.accuracy} meters.<br>`;
+      
+    document.getElementById("p1").innerHTML = str;
+    
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+  
+    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+  
+    /* global Image */
+    var img = new Image();
+    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+  
+    output.appendChild(img);
+  
+  }
+  
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+    output.innerHTML = "Unable to retrieve your location";
+  }
+  
+  /* global navigator */
+  navigator.geolocation.getCurrentPosition(success, error, options);
+};
+
